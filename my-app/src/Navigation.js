35052e3246d2f988c99e4d6e34a15b9f4cd41b94
Nhat Ho <honhattan2411@gmail.com>
2019-12-17 22:02:39 -0800

    Creating Navigation bar

---
+++ my-app/src/Navigation.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Navigation.css';
+
+//React.Component
+class Navigation extends Component { 
+  render() {
+    const sections = ['Home', 'About', 'Courses', 'Youtube', 'Linkedin', 'Github'];
+    const navLinks = sections.map(section=>{
+        return (
+            <li><a href={'#' + section }>{section}</a></li>
+        )
+    });
+    return (
+        <nav>
+            <h2 className="logo" >{this.props.logoTitle}</h2>
+            <ul>
+              <li><a href="#" target="_blank">Home</a></li>
+              <li><a href="#" target="_blank">About</a></li>
+              <li><a href="#" target="_blank">Courses</a></li>
+              <li><a href="https://www.youtube.com/channel/UC6SmN_XPGFO5Sum5n1EVZjQ?view_as=subscriber" target="_blank">Youtube</a></li>
+              <li><a href="https://www.linkedin.com/in/tan-ho-7518a1a2/" target="_blank">Linkedin</a></li>
+              <li><a href="https://github.com/nhattanho" target="_blank">Github</a></li>
+            </ul>
+        </nav>
+    )
+  }
+}
+
+export default Navigation;
