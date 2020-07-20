0c386bd50b1f63c51e34514dd2144dbab6e9d8e7
Nhat Ho <honhattan2411@gmail.com>
2020-07-20 00:41:38 -0700

    Understanding about Asynchronous setState

---
+++ Practice_React/3.async-setstate/src/App.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  //Using ES7 ==> It is shorter than using ES6. And props passed from file index.js will automatically be understood in this file
+  // state = {
+  //   number: 50 + this.props.incre
+  // };
+
+  //=> Come back to use ES6 with passing the props from the index.js
+  // constructor(props){ 
+  //   super(props);
+  //   this.state = {
+  //     number: 50
+  //   };
+  //   this.props = props;
+  // }
+
+  //=> Come back to use ES6, with don't need to pass props
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     //number: 50 + this.props.incre ==> wrong, even though the props will be automatically understood by this file, but if we want to use in the constructor
+  //     //we have to pass it into the constructor function, like the code as below:
+  //     number: 50
+  //   };
+  // }
+
+  // constructor(props){ 
+  //   super(props);
+  //   this.state = {
+  //     number: 50 + this.props.incre
+  //   };
+  //   //this.props = props;
+  // }
+
+  // However, with ES7, we don't need to pass the props, it will handle all of things for us:
+  state = {
+    number: 50 + this.props.incre // props is equal level with state, and we are in the method of state object, so we need
+    //to use "this" to point the props ==> make sense!
+  };
+  
+  // handleChange = ()=>{
+  //     this.setState({number: this.state.number + this.props.incre}, ()=> { // function setState work in background <=> asynchronous
+  //     console.log(this.state.number);
+  // })};
+
+  // with setState,we can pass a function which likes a callback funtion, we can get an example as below:
+  handleChange = ()=>{
+    this.setState( (stateAr, propsAr) => { // stateAr <=> this.state, propsAr <=> this.props
+      return {number: stateAr.number + propsAr.incre}; // have to return an object to update for the state
+    }, () => {
+      console.log(this.state.number);
+    } );
+  };
+    
+  //   /*
+  //   this.setState({number: this.state.number+1});
+  //   console.log(`this.state.number outside of setState: ${this.state.number}`);
+  //   */
+  // 
+    
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>{this.state.number}</p>
+          {/*tring(this.state.number)*/}
+          <button onClick={this.handleChange}> Click Me </button>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
