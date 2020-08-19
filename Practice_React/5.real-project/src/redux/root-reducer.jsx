798553a02380a69fed840872a755b7b310e6623f
Nhat Ho <honhattan2411@gmail.com>
2020-08-18 20:20:15 -0700

    Using Persist to save data on storage

--- Practice_React/5.real-project/src/redux/root-reducer.jsx
+++ Practice_React/5.real-project/src/redux/root-reducer.jsx
@@ -1,9 +1,24 @@
 import {combineReducers} from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // default storage
 
-console.log('in root reducer');
-export default combineReducers ({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']// because the user was handled by authoStateChange by firebase, so we don't need use persist to storage it
+}
+
+// console.log('in root reducer');
+// export default combineReducers ({
+//     user: userReducer,
+//     cart: cartReducer
+// });
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- Practice_React/5.real-project/src/redux/store.js
+++ Practice_React/5.real-project/src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
-
+import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
 
 //setup for middleware
 const middlewares = [logger];// array
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
